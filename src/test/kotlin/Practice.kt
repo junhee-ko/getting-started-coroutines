5a501088f185988120c4ab2ac5b92d5d109e454a
junhee-ko <junheee.ko@gmail.com>
2023-05-21 18:04:24 +0900

    Add 'Practice' test
    - https://developer.android.com/codelabs/basic-android-kotlin-training-introduction-coroutines#5

---
+++ src/test/kotlin/Practice.kt
@@ -0,0 +1,40 @@
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import org.junit.jupiter.api.Test
+
+class Practice {
+
+    @Test
+    fun `Creating and running multiple threads`() {
+        val states = arrayOf("Starting", "Doing Task 1", "Doing Task 2", "Ending")
+
+        repeat(3) {
+            Thread {
+                println("${Thread.currentThread()} has started")
+                for (i in states) {
+                    println("${Thread.currentThread()} - $i")
+                    Thread.sleep(50)
+                }
+            }.start()
+        }
+
+        Thread.sleep(3000)
+    }
+
+    // asis: Creating and running multiple threads
+    @Test
+    fun `rewrite the code to use coroutines instead of Thread`() {
+        val states = arrayOf("Starting", "Doing Task 1", "Doing Task 2", "Ending")
+
+        repeat(10) {
+            GlobalScope.launch {
+                println("${Thread.currentThread()} has started")
+                for (i in states) {
+                    println("${Thread.currentThread()} - $i")
+                }
+            }
+        }
+
+        Thread.sleep(3000)
+    }
+}
