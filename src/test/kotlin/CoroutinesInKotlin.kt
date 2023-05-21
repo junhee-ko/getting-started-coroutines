4a00ac746a3e7496efef1b674ac4c47826536001
junhee-ko <junheee.ko@gmail.com>
2023-05-21 18:04:09 +0900

    Add 'Coroutines in Kotlin' test
    - https://developer.android.com/codelabs/basic-android-kotlin-training-introduction-coroutines#3

---
+++ src/test/kotlin/CoroutinesInKotlin.kt
@@ -0,0 +1,45 @@
+import kotlinx.coroutines.*
+import org.junit.jupiter.api.Test
+import java.time.LocalDateTime
+
+class CoroutinesInKotlin {
+
+    @Test
+    fun `use coroutines`() {
+        repeat(3) {
+            GlobalScope.launch {
+                println("Hi from ${Thread.currentThread()}")
+            }
+        }
+
+        Thread.sleep(3000)
+    }
+
+    @Test
+    fun `A word about runBlocking`() {
+        runBlocking {
+            val num1: Double = getValue()
+            val num2: Double = getValue()
+            println("result of num1 + num2 is ${num1 + num2}")
+        }
+    }
+
+    @Test
+    fun `A word about runBlocking with async`() {
+        runBlocking {
+            val num1: Deferred<Double> = async { getValue() }
+            val num2: Deferred<Double> = async { getValue() }
+            println("result of num1 + num2 is ${num1.await() + num2.await()}")
+        }
+    }
+
+    // Whenever a function calls another suspend function, then it should also be a suspend function.
+    suspend fun getValue(): Double {
+        println("entering getValue() at ${LocalDateTime.now()}")
+        delay(3000) // suspend function
+        println("leaving getValue() at ${LocalDateTime.now()}")
+
+        return Math.random()
+    }
+
+}
