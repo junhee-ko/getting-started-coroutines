74cc9589a923dbec1ca9607ef649485e07d3550e
junhee-ko <junheee.ko@gmail.com>
2023-05-21 18:03:50 +0900

    Add 'Challenges with threads' test
    - https://developer.android.com/codelabs/basic-android-kotlin-training-introduction-coroutines#2

---
+++ src/test/kotlin/ChallengesWithThreads.kt
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.Test
+
+class ChallengesWithThreads {
+
+    @Test
+    fun `Race conditions and unpredictable behavior`() {
+        var count = 0
+        for (i in 1..50) {
+            Thread {
+                count += 1
+                println("Thread: $i count: $count")
+            }.start()
+        }
+
+        Thread.sleep(2000)
+    }
+}
