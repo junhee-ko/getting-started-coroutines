3b45305fe09d8689392d1b7eda40a2ab962276d1
junhee-ko <junheee.ko@gmail.com>
2023-06-02 22:21:08 +0900

    Update StructuredConcurrency_1-2
    - 코루틴 빌더는 다른 코루틴 내에서 호출되어야한다

--- src/test/kotlin/fastcampus/StructuredConcurrency_1-2.kt
+++ src/test/kotlin/fastcampus/StructuredConcurrency_1-2.kt
@@ -5,7 +5,33 @@ import org.junit.jupiter.api.Test
 
 class `StructuredConcurrency_1-2` {
 
+    // 제대로 수행 되지 않음
+    // 코루틴 빌더는 runBlocking 을 제외하고는 다른 코루틴 내에서 호출해야한다
+    // 즉, 아래의 launch 는 코루틴 내에서 호출되어야한다
+//    private suspend fun doOneTwoThreeNotWorking() {
+//        launch {
+//            println("launch1: ${Thread.currentThread()}")
+//            delay(200L)
+//            println("3!")
+//        }
+//
+//        launch {
+//            println("launch2: ${Thread.currentThread()}")
+//            delay(2000L)
+//            println("1!")
+//        }
+//
+//        launch {
+//            println("launch3: ${Thread.currentThread()}")
+//            delay(3000L)
+//            println("2!")
+//        }
+//
+//        println("4!")
+//    }
+
     private suspend fun doOneTwoThree() {
+        // coroutineScope 는 코루틴 빌더 (여기서는 launch) 를 호출하기 위해서 존재
         // 부모 코루틴은 자식이 끝날 때 까지 기다린다
         // 즉, 아래 세 개의 launch 가 끝날 때 까지 기다림
         coroutineScope { // this: 코루틴
@@ -34,7 +60,7 @@ class `StructuredConcurrency_1-2` {
     @Test
     fun `코루틴 빌더를 suspend 함수 안에서 호출`() {
         runBlocking { // this: 코루틴. Receiver. 수신객체
-            doOneTwoThree() // suspension point
+            doOneTwoThree() // suspension point == 잠들고 양보를 함
             println("runBlocking: ${Thread.currentThread()}")
             println("5!")
         }
