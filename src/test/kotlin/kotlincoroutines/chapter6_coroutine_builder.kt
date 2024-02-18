742d7f8b73bcf93eeb8b60dbecd3cc63bea9359a
junhee-ko <junheee.ko@gmail.com>
2024-02-18 15:10:59 +0900

    Update chapter6_coroutine_builder.kt

--- src/test/kotlin/kotlincoroutines/chapter6_coroutine_builder.kt
+++ src/test/kotlin/kotlincoroutines/chapter6_coroutine_builder.kt
@@ -9,16 +9,16 @@ class Chapter6 {
     @Test
     fun launch() {
         GlobalScope.launch {
-            delay(1000L)
             log("1")
+            delay(1000L)
         }
         GlobalScope.launch {
-            delay(1000L)
             log("2")
+            delay(1000L)
         }
         GlobalScope.launch {
-            delay(1000L)
             log("3")
+            delay(1000L)
         }
 
         log("4")
@@ -43,6 +43,26 @@ class Chapter6 {
     }
 
     @Test
+    fun `runBlocking 안에서 delay 를 사용`() {
+        runBlocking {
+            GlobalScope.launch {
+                delay(1000)
+                log(1)
+            }
+            GlobalScope.launch {
+                delay(1000)
+                log(2)
+            }
+            GlobalScope.launch {
+                delay(1000)
+                log(3)
+            }
+            log(4)
+            delay(2000)
+        }
+    }
+
+    @Test
     fun `async 코루틴 빌더는 값을 생성한다`() {
         runBlocking {
             val deferred: Deferred<Int> = GlobalScope.async {
@@ -58,24 +78,41 @@ class Chapter6 {
     fun `값이 생성되기 전에 await 를 호출하면 값이 나올 때 까지 기다린다`() {
         runBlocking{
             val result1 = GlobalScope.async {
+                log(1)
                 delay(5000L)
                 1
             }
             val result2 = GlobalScope.async {
+                log(2)
                 delay(9000L)
                 2
             }
             val result3 = GlobalScope.async {
+                log(3)
                 delay(7000L)
                 3
             }
 
+            log(4)
             log(result1.await().toString())
             log(result2.await().toString())
             log(result3.await().toString())
         }
     }
 
+    @Test
+    fun `async 는 값을 생성할 때 사용되며, 값이 필요하지 않을 때는 launch 를 써야한다`() {
+        runBlocking{
+            GlobalScope.async {
+                delay(1000)
+                log(1)
+            }
+            log(2)
+            delay(2000)
+        }
+    }
+
+    // 구조화된 동시성
     // 자식은 부모로부터 컨텍스트를 상속받는다.
     // 부모는 자식이 작업을 마칠 때까지 기다린다.
     // 부모 코루틴이 취쇠되면 자식 코루틴도 취소된다.
